6647657b62ffe8192ad67fcc129aea30fdd748f3
alvarorenan <alvaroca1544@gmail.com>
2024-08-21 16:04:26 -0300

    feat: Adicionar modelo Matricula e serializer
    Adiciona o modelo Matricula e o serializer correspondente para permitir a criação e manipulação de matrículas de estudantes em cursos.

--- setup/urls.py
+++ setup/urls.py
@@ -1,14 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path,include
-from escola.views import EstudanteViewSet, CursoViewSet
+from escola.views import EstudanteViewSet, CursoViewSet, MatriculaViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 
 router.register('estudantes', EstudanteViewSet, basename='Estudante')
 router.register('cursos', CursoViewSet, basename='Curso')
-
+router.register('matriculas', MatriculaViewSet, basename='Matricula')
 
 
 urlpatterns = [
