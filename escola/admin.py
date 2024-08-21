4051ae7d327868196ece7fad8d6df30fc2eae359
alvarorenan <alvaroca1544@gmail.com>
2024-08-21 15:31:23 -0300

    chore: Adicionar arquivos iniciais do projeto Django

---
+++ escola/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from escola.models import Estudante, Curso
+
+class EstudanteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'email', 'cpf', 'data_nascimento', 'celular')
+    list_display_links = ('id', 'nome',)
+    list_per_page = 20
+    search_fields = ('nome',)
+
+admin.site.register(Estudante, EstudanteAdmin)
+
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('id','codigo', 'descricao', 'nivel')
+    list_display_links = ('id', 'codigo')
+    list_per_page = 20
+    search_fields = ('codigo',)
+
+admin.site.register(Curso, CursoAdmin)
---
+++ setup/urls.py
@@ -0,0 +1,17 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from escola.views import EstudanteViewSet, CursoViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+router.register('estudantes', EstudanteViewSet, basename='Estudante')
+router.register('cursos', CursoViewSet, basename='Curso')
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+]
